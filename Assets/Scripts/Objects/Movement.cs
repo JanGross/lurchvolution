f511f2bbbf2e78b9217ff9df0fd29f0f8d4cf18f
Jan Groß <minzkraut@gmail.com>
2018-09-01 20:49:43 +0200

    Platform movement and elimination zones
    Adds scripts for moving any object up/down, left/right or  forwards/backwars and eliminating the player.

---
+++ Assets/Scripts/Objects/Movement.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Movement : MonoBehaviour {
+
+    [System.Serializable]
+    class MovementDirection
+    {
+        [SerializeField]
+        private bool enabled = false;
+        [SerializeField]
+        private enum Direction { Left_Right, Up_Down, Fwd_Bkw }
+        [SerializeField]
+        private Direction direction;
+        [SerializeField]
+        private bool loop = false;
+        [SerializeField]
+        private bool goBack = false;
+        [SerializeField]
+        private float distance = 100;
+        [SerializeField]
+        private float speed = 0;
+        [SerializeField]
+        private float dist;
+
+        private Vector3 initialPosition;
+        public void SetInitialPosition(Vector3 initPos)
+        {
+            initialPosition = initPos;
+        }
+
+        public Vector3 Move(Vector3 dirIn)
+        {
+            int dir = (int)direction;
+            Vector3 dirOut = dirIn;
+            enabled = loop ? true : enabled;
+
+            if (enabled)
+            {
+                dist = initialPosition[dir] - dirOut[dir];
+
+                if (goBack)
+                {
+                    dirOut[dir] += speed * Time.deltaTime;
+                }
+                else
+                {
+                    dirOut[dir] -= speed * Time.deltaTime;
+                }
+
+                if (dist > distance && !goBack)
+                {
+                    if (loop)
+                    {
+                        goBack = true;
+                    }
+                    else
+                    {
+                        enabled = false;
+                    }
+                }
+                else if (dist <= 0)
+                {
+                    goBack = false;
+
+                }
+            }
+            return dirOut;
+        }
+    }
+
+    [SerializeField]
+    private List<MovementDirection> directions;
+	// Use this for initialization
+	void Start () {
+        foreach (MovementDirection direction in directions)
+        {
+            direction.SetInitialPosition(transform.position);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		foreach ( MovementDirection direction in directions)
+        {
+            transform.position = direction.Move(transform.position);
+        }
+	}
+}
---
+++ Assets/Scripts/TriggerEffects/EliminatePlayer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EliminatePlayer : MonoBehaviour {
+
+    [SerializeField]
+    private string name = "Generic trap";
+    [SerializeField]
+    private float resapwnDelay = 0;
+    [Tooltip("Optional particle effect. Spawned when player enters the trigger")]
+    [SerializeField]
+    private GameObject effect;
+    [SerializeField]
+    private LurchRespawn player;
+    [SerializeField]
+    private bool killQueued = false;
+    [SerializeField]
+    private float rdCounter = 0;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (rdCounter <= 0 && killQueued)
+        {
+            player.RespawnLurch(name);
+            killQueued = false;
+        } else
+        {
+            rdCounter -= Time.deltaTime;
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.root.tag == "Player")
+        {
+            rdCounter = resapwnDelay;
+            killQueued = true;
+            if(effect != null)
+            {
+                GameObject.Instantiate(effect);
+            }
+        }
+    }
+}
